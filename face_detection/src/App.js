12eca1d69bc1aadfe7769d0590728265a489427a
Deniz Evrendilek <devrendilek@gmail.com>
2019-08-29 12:49:50 -0700

    added particle.js and edited tilt.js css

--- face_detection/src/App.js
+++ face_detection/src/App.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Nav from './comps/navs/nav';
 import './App.css';
 import Logo from './comps/logo/logo.js';
 import ImageLink from './comps/imageLink/image_link';
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-    <Nav />
-    <Logo />
-    <ImageLink />
-    {/* <FaceRecognition /> */}
-    </div>
-  );
+const particleParams = {
+  particles: {
+    number:{
+      value:150,
+      density:{
+        enable:true,
+        value_area:2000
+      }
+    },
+    color:{
+      value: "#ffffff"
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+      <Particles className='particles'
+      params={particleParams}
+      />
+      <Nav />
+      <Logo />
+      <ImageLink />
+      {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
