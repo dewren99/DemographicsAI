cb09dcb7a4b7697ff259cff6d16f1cde10b99b9b
Deniz Evrendilek <devrendilek@gmail.com>
2019-08-29 01:41:09 -0700

    Installed React and Created JS files

---
+++ face_detection/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from './comps/navs/nav';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Nav />
+    {/* <Logo />
+    <ImageLink />
+    <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ face_detection/src/comps/navs/nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () =>{
+    return(
+        <nav>
+        <p>
+            Sign Out
+        </p>
+        </nav>
+    );
+}
+
+export default Nav;
