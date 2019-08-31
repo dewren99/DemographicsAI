2f85c95999d6688ebe86c9cc5fb1307d0e19a7fc
Deniz Evrendilek <devrendilek@gmail.com>
2019-08-31 02:11:40 -0700

    fixed formatting on signin.js and added npm package for password protection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
+
 const app = express();
 
 const database = {
@@ -18,17 +22,35 @@ const database = {
 app.get('/', (request, response) => {})
 
 app.post('/signin', (request, response) => {
-    if(true){
-
-    }
-    else{
-        response.status(400).json(`Error Logging in`);
+    if (true) {} else {
+        response
+            .status(400)
+            .json(`Error Logging in`);
     }
 })
 
 app.post('/signup', (request, response) => {
     const {email, name, password} = request.body;
     response.json('');
+})
+
+app.get('profile/:id', (request, response) => {
+    const {id} = request.params;
+    let found = false;
+    database
+        .users
+        .forEach(users => {
+            if (users.id === id) {
+                response.json(user);
+                found == true;
+
+            }
+        })
+    if (!found) {
+        response
+            .status(404)
+            .json('This user does not exists!');
+    }
 })
 
 app.listen(3002, () => {})
